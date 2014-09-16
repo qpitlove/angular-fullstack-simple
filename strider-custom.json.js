886c23d83ff552f3158d84e5afd0edcbf6aa23e8
qpitlove <qpitlove@gmail.com>
2014-09-16 17:41:22 +0900

    strider ci 수정

---
+++ strider-custom.json.js
@@ -0,0 +1,8 @@
+/**
+ * Created by irteam on 2014. 9. 16..
+ */
+{
+  "prepare": "echo executed prepare statement!",
+  "test": "echo executed deploy statement!",
+  "deploy": "echo deploy && git push heroku master --force"
+}
